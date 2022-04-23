9cca9bf7bd826c49848c7645e6146fd25d68b571
liorAbuhav <lior290999@gmail.com>
2022-04-24 01:16:43 +0300

    schedule api calling for monitoring

---
+++ main.py
@@ -0,0 +1,22 @@
+import os, time, requests, json
+from apscheduler.schedulers.blocking import BlockingScheduler
+  
+def get_all_monitors():
+    monitorsResponse = requests.get(os.environ.get('API-URL') + 'monitor')
+    monitors = json.loads(monitorsResponse.text)
+    return monitors
+
+def execute_monitors():
+    monitors = get_all_monitors()
+    for monitor in monitors:
+        executeResponse = requests.post(os.environ.get('API-URL') + 'executor/execute/' + monitor["_id"])
+        print(executeResponse)
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(execute_monitors, 'interval', hours=1)
+scheduler.start()
+
+
+
+  
